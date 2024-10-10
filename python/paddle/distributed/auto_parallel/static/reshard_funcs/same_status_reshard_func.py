c029e62a39afa3732b673e9064e13ac14a88ade5
xuexixi <xuexixi@baidu.com>
2024-10-10 20:01:52 +0800

    Set op execution_stream as AutoParallelStreamType

--- python/paddle/distributed/auto_parallel/static/reshard_funcs/same_status_reshard_func.py
+++ python/paddle/distributed/auto_parallel/static/reshard_funcs/same_status_reshard_func.py
@@ -13,7 +13,10 @@
 # limitations under the License.
 
 import paddle
-from paddle.distributed.passes.pass_utils import find_var_used_op_chunk_id
+from paddle.distributed.passes.pass_utils import (
+    AutoParallelStreamType,
+    find_var_used_op_chunk_id,
+)
 
 from ..process_group import new_process_group
 from .base_reshard_func import ReshardFunction
@@ -112,6 +115,9 @@ class SameStatusReshardFunction(ReshardFunction):
                         src_mesh, [src_dist_attr], [], chunk_id
                     )
                 )
+                new_op.set_execution_stream(
+                    AutoParallelStreamType.CALC_STREAM.value
+                )
                 break
 
             elif dst == cur_global_rank:
@@ -143,6 +149,9 @@ class SameStatusReshardFunction(ReshardFunction):
                 )
                 recv_value.set_type(dst_type)
                 is_send = False
+                new_op.set_execution_stream(
+                    AutoParallelStreamType.CALC_STREAM.value
+                )
                 break
 
         if is_send:
